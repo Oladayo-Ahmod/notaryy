682e58d1f64fa529a13a8b0d2b86a301193cf46c
Oladayo Ahmod <oladayoahmod112@gmail.com>
2024-02-27 10:24:05 +0100

    contract deployment script

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -1,10 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require('hardhat-deploy')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.0",
+    defaultNetwork: "hardhat",
     networks: {
+        hardhat: {
+            chainId: 31337,
+        },
         alfajores: {
             url: "https://alfajores-forno.celo-testnet.org",
             accounts: [process.env.PRIVATE_KEY],
@@ -37,5 +42,12 @@ module.exports = {
                 },
             },
         ],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+       
     },
 };
